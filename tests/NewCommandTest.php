9f9fa56c934133bb0b0df7a00d35a5831f80bf60
Benedict <ben@benedict-wallis.com>
2022-03-22 15:01:25 +0000

    Initial Commit

---
+++ tests/NewCommandTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PressGang\Installer\Console\Tests;
+
+use PressGang\Installer\Console\NewCommand;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class NewCommandTest extends TestCase
+{
+    public function test_it_can_scaffold_a_new_pressgang_theme()
+    {
+        $scaffoldDirectoryName = 'tests-output/my-theme';
+        $scaffoldDirectory = __DIR__.'/../'.$scaffoldDirectoryName;
+
+        if (file_exists($scaffoldDirectory)) {
+            if (PHP_OS_FAMILY == 'Windows') {
+                exec("rd /s /q \"$scaffoldDirectory\"");
+            } else {
+                exec("rm -rf \"$scaffoldDirectory\"");
+            }
+        }
+
+        $app = new Application('PressGang Installer');
+        $app->add(new NewCommand);
+
+        $tester = new CommandTester($app->find('new'));
+
+        $statusCode = $tester->execute(['name' => $scaffoldDirectoryName]);
+
+        $this->assertSame(0, $statusCode);
+        $this->assertDirectoryExists($scaffoldDirectory.'/vendor');
+    }
+}
